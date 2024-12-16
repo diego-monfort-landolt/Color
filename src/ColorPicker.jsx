68bf7e7389a3bea077db3502bfc6ef0769ba5ef7
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-12-16 19:47:34 +0100

    removed style -add color code

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -3,12 +3,12 @@ import ColorBox from './ColorBox';
 import './colorPicker.css';
 
 const colors = [
-  '#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF33A1', '#33FFF5', '#F5FF33', '#FF8C33'
+  '#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF33A1', '#33FFF5', '#F5FF33', '#FF8C33', 	'#80ffe9', '#ffb3e6', '#020002', '#fff'
 ];
 
 const ColorPicker = () => {
   return (
-    <div className="color-picker" style={{ backgroundColor: colors}}>
+    <div className="color-picker">
       {colors.map(color => (
         <ColorBox key={color} color={color} />
       ))}
